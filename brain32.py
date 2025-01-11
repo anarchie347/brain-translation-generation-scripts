8f2ad1b24d76290fc58892a0ac4851824d67c586
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2025-01-11 17:23:11 +0000

    added untested subtraction, minor performance improvements to add

--- brain32.py
+++ brain32.py
@@ -53,16 +53,16 @@ def translation_add_full(): # starts W0, ends W0
     add_code += "]"
 
     add_code += copy_Dx_to_W0(1)
-    add_code += "[<<<<<[>>>>>" # W0(=D1) != 0 and W[-1] != 0 then undo carries in D2,3
-    add_code += "<<<-<-"
+    add_code += "[<<<<<[" # W0(=D1) != 0 and W[-1] != 0 then undo carries in D2,3
+    add_code += ">>-<-"
     add_code += "<" + SET_ZERO # set W[-1] = 0 so no more undo-carries done (and exit loop)
     add_code += "]" # exit W[-1]
     add_code += ">>>>>" + SET_ZERO
     add_code += "]" # exit W0
 
     add_code += copy_Dx_to_W0(2)
-    add_code += "[<<<<<[>>>>>" # W0(=D2) != 0 and W[-1] != 0 then undo carries in D3
-    add_code += "<<<<-"
+    add_code += "[<<<<<[" # W0(=D2) != 0 and W[-1] != 0 then undo carries in D3
+    add_code += ">-"
     add_code += "<" + SET_ZERO # set W[-1] = 0 so no more undo-carries done (and exit loop)
     add_code += "]" # exit W[-1]
     add_code += ">>>>>" + SET_ZERO
@@ -71,32 +71,55 @@ def translation_add_full(): # starts W0, ends W0
 
     return add_code
 
-def translation_sub_full(): # ive forgotten how loops work
-    sub_code = copy_Dx_to_W0(0)
-    sub_code += "["
+def translation_sub_full():
+    sub_code = "<<<<<" + SET_ZERO + "+>>>>>" # set W[-1] = 1, used so only 1 undo carry block is run
+    sub_code += copy_Dx_to_W0(0)
+    sub_code += "[" # if W0(=D0) != 0 then undo carry on D1,2,3
+    sub_code += "<<+<+<+"
+    sub_code += "<" + SET_ZERO + ">>>>>" # set W[-1] = 0 to prevent future undos
+    sub_code += SET_ZERO
+    sub_code += "]" # exit W0
+
     sub_code += copy_Dx_to_W0(1)
-    sub_code += "["
+    sub_code += "[<<<<<[" # if W0(=D1) !=0 and havent already undone, undo carry on D2,3
+    sub_code += ">>+<+<"
+    sub_code += SET_ZERO # set W[-1] = 0 to prevent future carries
+    sub_code += "]"
+    sub_code += ">>>>>" + SET_ZERO # zero W0 to exit loop
+    sub_code += "]" # exit on W0
+
     sub_code += copy_Dx_to_W0(2)
-    sub_code += "["
-    sub_code += "<<<<->>>>"
-    sub_code += SET_ZERO
+    sub_code += "[<<<<<[" # if W0(=D2) !=0 and havent already undone, undo carry on D3
+    sub_code += ">+<"
+    sub_code += SET_ZERO # zero W[-1] to exit loop
     sub_code += "]"
-    sub_code += "<<<->>>"
-    sub_code += SET_ZERO
-    sub_code += "]"
-    sub_code += "<<->>"
-    sub_code += SET_ZERO
-    sub_code += "]"
-    sub_code += "-"
+    sub_code += SET_ZERO + ">>>>>" # zero W0 to exit loop
+    sub_code += "]" # exit W0
+
+    sub_code += "<-<-<-<->>>>" # do actual subtraction. Has to be done after undo-carries because special case is when value is 0 BEFORE subtracting
     return sub_code
 
 ADVANCE_COLON = ">>>>>>>>>>>>>>:<<<<<<<<<<<<<<"
 AROUND_COLON = ">>>>>>>:<<<<<<<"
 
-
+def gen_add_tests():
+    result = ">"
+    result += ">>>>"
+    for i in range(0,16):
+        result += "<<<<"
+        for place in range (0,4):
+            if ((i >> place) & 1):
+                result += "->"
+            else:
+                result += "+" * 5 + ">"
+        result += AROUND_COLON
+        result += translation_add_full()
+        result += AROUND_COLON
+        result += "<[-]" * 4 + ">" * 4 + "\\"
+    return result
 
 # actual code
-result = ""
+result = gen_add_tests()
 
 
 with open("output.bf", "w") as file:
